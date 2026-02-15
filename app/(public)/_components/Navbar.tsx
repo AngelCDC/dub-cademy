4a8f574f8f13d4ba9d4b52a35d9eb52c701c706d
AngelCDC <angelcdc06@gmail.com>
2026-02-15 16:31:50 -0400

    logout navbar

--- app/(public)/_components/Navbar.tsx
+++ app/(public)/_components/Navbar.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ThemeToggle } from "@/components/ui/themeToggle";
 import { authClient } from "@/lib/auth-client";
 import { UserDropdown } from "./UserDropdown";
 import { useSignOut } from "@/hooks/use-singout";
@@ -37,7 +36,7 @@ export function Navbar() {
             <li key={item.name}>
               <Link
                 href={item.href}
-                className={`nav-link max-xl:text-[0.85rem] ${pathname === item.href ? "nav-link-active" : ""}`}
+                className={`nav-link max-xl:text-[0.85rem] ${pathname === item.href ? "text-accent-red" : "text-primary-black hover:text-accent-red"}`}
               >
                 {item.name}
               </Link>
@@ -143,26 +142,21 @@ export function Navbar() {
               <div className="text-center text-text-gray">Cargando...</div>
             ) : session ? (
               <>
-                <UserDropdown
-                  email={session.user.email}
-                  image={
-                    session?.user.image ??
-                    `https://avatar.vercel.sh/${session?.user.email}`
-                  }
-                  name={
-                    session?.user.name && session.user.name.length > 0
-                      ? session.user.name
-                      : session?.user.email.split("@")[0]
-                  }
-                />
-                <Link
-                  href="/dashboard"
-                  className={`block text-lg font-medium tracking-wider uppercase transition-colors text-primary-black hover:text-accent-red`}
-                  onClick={() => setMobileMenuOpen(false)}
-                >
-                  Dashboard
-                </Link>
-                <div className="login-button cta-primary" onClick={handleSignOut}>Logout</div>
+                <div className="space-y-6">
+                  <Link
+                    href="/dashboard"
+                    className={`block text-lg font-medium tracking-wider uppercase transition-colors text-primary-black hover:text-accent-red`}
+                    onClick={() => setMobileMenuOpen(false)}
+                  >
+                    Dashboard
+                  </Link>
+                  <div
+                    className="login-button cta-primary"
+                    onClick={handleSignOut}
+                  >
+                    Logout
+                  </div>
+                </div>
               </>
             ) : (
               <Link
